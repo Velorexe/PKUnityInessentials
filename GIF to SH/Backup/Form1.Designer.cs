fbb450802d080cdd5472349fbe000be37879cbc4
Velorexe <36154558+Velorexe@users.noreply.github.com>
2018-05-18 13:13:28 +0200

    Spritesheets centered, source for GIF to SH uploaded

---
+++ GIF to SH/Backup/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace Gif_to_Sheet
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblFile = new System.Windows.Forms.Label();
+            this.tbFile = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.lblColumns = new System.Windows.Forms.Label();
+            this.nudColumns = new System.Windows.Forms.NumericUpDown();
+            this.panelMain = new System.Windows.Forms.Panel();
+            this.pbSprite = new System.Windows.Forms.PictureBox();
+            this.pbBackColor = new System.Windows.Forms.PictureBox();
+            this.lblBackColor = new System.Windows.Forms.Label();
+            this.btnExport = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.nudColumns)).BeginInit();
+            this.panelMain.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSprite)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBackColor)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblFile
+            // 
+            this.lblFile.AutoSize = true;
+            this.lblFile.Location = new System.Drawing.Point(12, 9);
+            this.lblFile.Name = "lblFile";
+            this.lblFile.Size = new System.Drawing.Size(23, 13);
+            this.lblFile.TabIndex = 0;
+            this.lblFile.Text = "File";
+            // 
+            // tbFile
+            // 
+            this.tbFile.Location = new System.Drawing.Point(12, 25);
+            this.tbFile.Name = "tbFile";
+            this.tbFile.ReadOnly = true;
+            this.tbFile.Size = new System.Drawing.Size(189, 20);
+            this.tbFile.TabIndex = 1;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(207, 23);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 2;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // lblColumns
+            // 
+            this.lblColumns.AutoSize = true;
+            this.lblColumns.Location = new System.Drawing.Point(288, 9);
+            this.lblColumns.Name = "lblColumns";
+            this.lblColumns.Size = new System.Drawing.Size(47, 13);
+            this.lblColumns.TabIndex = 3;
+            this.lblColumns.Text = "Columns";
+            // 
+            // nudColumns
+            // 
+            this.nudColumns.Location = new System.Drawing.Point(291, 25);
+            this.nudColumns.Maximum = new decimal(new int[] {
+            15,
+            0,
+            0,
+            0});
+            this.nudColumns.Name = "nudColumns";
+            this.nudColumns.Size = new System.Drawing.Size(111, 20);
+            this.nudColumns.TabIndex = 4;
+            this.nudColumns.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.nudColumns.ValueChanged += new System.EventHandler(this.nudColumns_ValueChanged);
+            // 
+            // panelMain
+            // 
+            this.panelMain.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.panelMain.AutoScroll = true;
+            this.panelMain.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panelMain.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelMain.Controls.Add(this.pbSprite);
+            this.panelMain.Location = new System.Drawing.Point(12, 73);
+            this.panelMain.Name = "panelMain";
+            this.panelMain.Size = new System.Drawing.Size(390, 366);
+            this.panelMain.TabIndex = 5;
+            // 
+            // pbSprite
+            // 
+            this.pbSprite.Location = new System.Drawing.Point(0, 0);
+            this.pbSprite.Name = "pbSprite";
+            this.pbSprite.Size = new System.Drawing.Size(100, 50);
+            this.pbSprite.TabIndex = 0;
+            this.pbSprite.TabStop = false;
+            this.pbSprite.Visible = false;
+            // 
+            // pbBackColor
+            // 
+            this.pbBackColor.BackColor = System.Drawing.Color.Magenta;
+            this.pbBackColor.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbBackColor.Location = new System.Drawing.Point(13, 51);
+            this.pbBackColor.Name = "pbBackColor";
+            this.pbBackColor.Size = new System.Drawing.Size(16, 16);
+            this.pbBackColor.TabIndex = 6;
+            this.pbBackColor.TabStop = false;
+            this.pbBackColor.Click += new System.EventHandler(this.pbBackColor_Click);
+            // 
+            // lblBackColor
+            // 
+            this.lblBackColor.AutoSize = true;
+            this.lblBackColor.Location = new System.Drawing.Point(35, 51);
+            this.lblBackColor.Name = "lblBackColor";
+            this.lblBackColor.Size = new System.Drawing.Size(92, 13);
+            this.lblBackColor.TabIndex = 7;
+            this.lblBackColor.Text = "Background Color";
+            // 
+            // btnExport
+            // 
+            this.btnExport.Location = new System.Drawing.Point(207, 46);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(75, 23);
+            this.btnExport.TabIndex = 8;
+            this.btnExport.Text = "Export";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(414, 451);
+            this.Controls.Add(this.btnExport);
+            this.Controls.Add(this.lblBackColor);
+            this.Controls.Add(this.pbBackColor);
+            this.Controls.Add(this.panelMain);
+            this.Controls.Add(this.nudColumns);
+            this.Controls.Add(this.lblColumns);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.tbFile);
+            this.Controls.Add(this.lblFile);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "GIF to Spritesheet";
+            ((System.ComponentModel.ISupportInitialize)(this.nudColumns)).EndInit();
+            this.panelMain.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbSprite)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBackColor)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblFile;
+        private System.Windows.Forms.TextBox tbFile;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Label lblColumns;
+        private System.Windows.Forms.NumericUpDown nudColumns;
+        private System.Windows.Forms.Panel panelMain;
+        private System.Windows.Forms.PictureBox pbSprite;
+        private System.Windows.Forms.PictureBox pbBackColor;
+        private System.Windows.Forms.Label lblBackColor;
+        private System.Windows.Forms.Button btnExport;
+    }
+}
+
---
+++ GIF to SH/Backup/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace Gif_to_Sheet
+{
+    public partial class Form1 : Form
+    {
+
+        Image gif;
+        Bitmap bitmap;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "GIF Images (*.gif)|*.gif";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                gif = Image.FromFile(ofd.FileName);
+                tbFile.Text = ofd.FileName;
+                pbSprite.Visible = true;
+                FormatImage();
+            }
+        }
+
+        private void FormatImage()
+        {
+            if (gif == null)
+                return;
+            if (bitmap != null)
+                bitmap.Dispose();
+            FrameDimension frameSize = new FrameDimension(gif.FrameDimensionsList[0]);
+            Size imageSize = new Size(gif.Size.Width, gif.Size.Height);
+            int frames = gif.GetFrameCount(frameSize);
+            Console.WriteLine(frames);
+            int columns = (int)nudColumns.Value < 1 ? frames : (int)nudColumns.Value;
+            int rows = (int)Math.Ceiling((double)frames / columns);
+            bitmap = new Bitmap(columns * imageSize.Width, rows * imageSize.Height);
+            Graphics g = Graphics.FromImage(bitmap);
+            Brush brush = new SolidBrush(pbBackColor.BackColor);
+            g.FillRectangle(brush, new Rectangle(0, 0, bitmap.Width, bitmap.Height));
+            for (int i = 0; i < frames; i++)
+            {
+                gif.SelectActiveFrame(frameSize, i);
+                g.DrawImage(gif, i % columns * imageSize.Width, i / columns * imageSize.Height);
+            }
+            g.Dispose();
+            pbSprite.Image = bitmap;
+            pbSprite.Size = bitmap.Size;
+        }
+
+        private void nudColumns_ValueChanged(object sender, EventArgs e)
+        {
+            FormatImage();
+        }
+
+        private void pbBackColor_Click(object sender, EventArgs e)
+        {
+            ColorDialog cd = new ColorDialog();
+            cd.Color = pbBackColor.BackColor;
+            if (cd.ShowDialog() == DialogResult.OK)
+            {
+                pbBackColor.BackColor = cd.Color;
+                FormatImage();
+            }
+        }
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "PNG Files (*.png)|*.png|BMP Files (*.bmp)|*.bmp|JPEG Files (*.jpeg)|*.jpeg|GIF Files (*.gif)|*.gif";
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                ImageFormat format;
+                string ext = Path.GetExtension(sfd.FileName);
+                switch (ext)
+                {
+                    case ".png":
+                        format = ImageFormat.Png;
+                        break;
+                    case ".jpeg":
+                        format = ImageFormat.Jpeg;
+                        break;
+                    case ".bmp":
+                        format = ImageFormat.Bmp;
+                        break;
+                    case ".gif":
+                        format = ImageFormat.Gif;
+                        break;
+                    default:
+                        format = ImageFormat.Png;
+                        break;
+                }
+                pbSprite.Image.Save(sfd.FileName, format);
+            }
+        }
+    }
+}
